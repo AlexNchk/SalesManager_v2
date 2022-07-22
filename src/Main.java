69494ba6a76ffcf28cf4bf24397d25b6c1338cea
Alexandr Yakovlev <alexnataforever@gmail.com>
2022-07-22 22:44:09 +0300

    Average Sales

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        int [] sales = new int[] {15,0,25,10,38};
+        int[] sales = new int[]{15, 0, 25, 10, 38};
         SalesManager max = new SalesManager(sales);
         int maxFin = max.max();
-        System.out.println("Максимальное количество продаж составляет: "+maxFin);
+        SalesManager average = new SalesManager(sales);
+        int averageFin = average.averageSales();
+        System.out.println("Максимальное количество продаж составляет: " + maxFin);
+        System.out.println("Среднее количество продаж составляет: " + averageFin);
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,5 +1,6 @@
 public class SalesManager {
     protected int[] sales;
+
     public SalesManager(int[] sales) {
         this.sales = sales;
     }
@@ -12,5 +13,21 @@ public class SalesManager {
             }
         }
         return max;
+    }
+
+    public int averageSales() {
+        int max = sales[0];
+        int min = sales[0];
+        int sum = 0;
+        for (int sale : sales) {
+            sum = sum + sale;
+            if (sale > max) {
+                max = sale;
+            }
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return (sum - min - max) / (sales.length - 2);
     }
 }
